13a3bddca18100a70a76218ffc3735f9b887d3d6
HughesSeanKyle <khughessean@yahoo.com>
2022-11-19 13:57:31 +0200

    Keep track of proposals

--- contracts/CryptoDevsDAO.sol
+++ contracts/CryptoDevsDAO.sol
@@ -21,6 +21,10 @@ contract CryptoDevsDAO is Ownable {
         // voters - a mapping of CryptoDevsNFT tokenIDs to booleans indicating whether that NFT has already been used to cast a vote or not
         mapping(uint256 => bool) voters;
     }
+
+    mapping(uint256 => Proposal) public proposals;
+    // Number of proposals that have been created
+    uint256 public numProposals;
 }
 
 /**
